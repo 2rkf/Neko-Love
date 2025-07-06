a91c10b7d0370b5a5489ef686711f7e060faea36
Reina <notmarx.tech@gmail.com>
2025-07-06 20:03:25 +0700

    feat(api): Fix auto-incremental when database error

--- api/src/handlers/user_register.rs
+++ api/src/handlers/user_register.rs
@@ -9,7 +9,11 @@ use axum_extra::{
 };
 use std::env;
 
-use crate::{ApiResponse, app_state::AppState, models::auth::RegisterRequest};
+use crate::{
+    ApiResponse,
+    app_state::AppState,
+    models::{auth::RegisterRequest, user::User},
+};
 
 pub async fn register_user(
     State(state): State<AppState>,
@@ -50,8 +54,32 @@ pub async fn register_user(
         }
     };
 
-    let result =
-        sqlx::query("INSERT INTO users (email, password, username, nickname) VALUES (?, ?, ?, ?)")
+    let user = sqlx::query_as!(
+        User,
+        "SELECT * FROM users WHERE username LIKE ? OR email LIKE ?",
+        payload.username,
+        payload.email
+    )
+    .fetch_one(&state.pool)
+    .await;
+
+    match user {
+        Ok(_) => {
+            return (
+                StatusCode::BAD_REQUEST,
+                Json(ApiResponse {
+                    id: None,
+                    message: Some("This user already exists.".into()),
+                    status: StatusCode::BAD_REQUEST.as_u16(),
+                    success: false,
+                    url: None,
+                }),
+            );
+        }
+        Err(_) => {
+            let result = sqlx::query(
+                "INSERT INTO users (email, password, username, nickname) VALUES (?, ?, ?, ?)",
+            )
             .bind(&payload.email)
             .bind(&hashed_password)
             .bind(&payload.username)
@@ -59,30 +87,32 @@ pub async fn register_user(
             .execute(&state.pool)
             .await;
 
-    match result {
-        Ok(_) => {
-            return (
-                StatusCode::CREATED,
-                Json(ApiResponse {
-                    id: None,
-                    message: Some("User successfully registered.".into()),
-                    status: StatusCode::CREATED.as_u16(),
-                    success: true,
-                    url: None,
-                }),
-            );
-        }
-        Err(_) => {
-            return (
-                StatusCode::CONFLICT,
-                Json(ApiResponse {
-                    id: None,
-                    message: Some("User already exists or database error.".into()),
-                    status: StatusCode::CONFLICT.as_u16(),
-                    success: false,
-                    url: None,
-                }),
-            );
+            match result {
+                Ok(_) => {
+                    return (
+                        StatusCode::CREATED,
+                        Json(ApiResponse {
+                            id: None,
+                            message: Some("User successfully registered.".into()),
+                            status: StatusCode::CREATED.as_u16(),
+                            success: true,
+                            url: None,
+                        }),
+                    );
+                }
+                Err(_) => {
+                    return (
+                        StatusCode::CONFLICT,
+                        Json(ApiResponse {
+                            id: None,
+                            message: Some("User already exists or database error.".into()),
+                            status: StatusCode::CONFLICT.as_u16(),
+                            success: false,
+                            url: None,
+                        }),
+                    );
+                }
+            }
         }
     }
 }
